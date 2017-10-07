f678887797c63a2de310eac65fd0c2cae2f4e99a
Sergei <zubkov@smoothie.digital>
2017-10-07 16:06:25 +0300

    Услуги разводящая: подключил api

--- frontend/static/js/views/Services.vue
+++ frontend/static/js/views/Services.vue
@@ -2,75 +2,11 @@
     <div class="services">
         <div class="services__wrapper">
             <div class="services__title">Услуги</div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
+            <div class="services__item" v-for="item in resultServices">
+                <div class="services__item-img" :style="`background-image: url('` + item.cover + `');`"></div>
                 <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
-                </div>
-
-            </div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
-                <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
-                </div>
-
-            </div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
-                <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
-                </div>
-
-            </div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
-                <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
-                </div>
-
-            </div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
-                <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
-                </div>
-
-            </div>
-            <div class="services__item">
-                <div class="services__item-img" style="background-image: url('/static/img/services1.jpg');"></div>
-                <div class="services__item_text">
-                    <div class="services__item_text-title">Реггинг</div>
-                    <div class="services__item_text-desc">
-                        Риггинг – это процесс монтажа несущих конструкций являющихся
-                        основой для любого мероприятия, будь то телесъёмка, концерт, танцевальный фестиваль. Компания
-                        Blackout Studio обладает всеми необходимыми допусками и сертификатами для работы на любой площадке мира.
-                    </div>
+                    <div class="services__item_text-title">{{ item.name }}</div>
+                    <div class="services__item_text-desc">{{ item.short_description }}</div>
                 </div>
 
             </div>
@@ -79,7 +15,31 @@
 </template>
 
 <script>
-  export default {
+  import axios from 'axios'
 
+  export default {
+    data() {
+      return {
+        resultServices: []
+      }
+    },
+    methods: {
+      getServices() {
+        let self = this
+
+        axios.get('/api/services/')
+          .then(
+            function (response) {
+              self.resultServices = response.data
+            },
+            function (error) {
+              console.log(error)
+            }
+          )
+      }
+    },
+    created(){
+      this.getServices()
+    }
   }
 </script>
