eea77a956bfbf5005d40cf0b3e92df40cac86ce5
yayayo <g.voytko@yandex.ru>
2017-10-10 21:21:14 +0300

    user bd

---
+++ config/production.py
@@ -0,0 +1,29 @@
+from config.settings import *
+DEBUG = False
+ALLOWED_HOSTS = ['www.testblackout.pythonanywhere.com', 'testblackout.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blackout',
+        'PASSWORD': 'user1111',
+        'HOST': 'testblackout-551.postgres.pythonanywhere-services.com',
+        'USER': 'super',
+    }
+}
+
+ADMINS = [
+    ('Blackout', 'zubkov@smoothie.digital')
+]
+
+SITE_HOST = 'testblackout.pythonanywhere.com'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+SERVER_EMAIL = EMAIL_HOST_USER
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
