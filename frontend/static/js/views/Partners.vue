2714d961be8268e3a5633d230fd71b5f7818abc7
Sergei <zubkov@smoothie.digital>
2017-10-10 22:16:21 +0300

    subcategory serializer

--- frontend/static/js/views/Partners.vue
+++ frontend/static/js/views/Partners.vue
@@ -29,7 +29,7 @@
         <div class="partners__item">
             <div class="partners__item_title">Производители оборудования</div>
             <div class="partners__item_icon">
-                <img src="/static/img/promoters.png" class="partners__item_icon-img" alt="">
+                <img src="/static/img/LOGOS4.png" class="partners__item_icon-img" alt="">
                 <img src="/static/img/pro2.png" class="partners__item_icon-img" alt="">
             </div>
             <div class="partners__item_desc">Мы поддерживаем постоянный контакт и регулярно обмениваемся информацией с производителями нашего оборудования. Это сотрудничество затрагивает многие аспекты работы и приносит пользу обеим сторонам. Помимо помощи в ремонте оборудования, технической поддержки и совместных пресс-релизов, производители совершенствуют свои приборы с учетом наших пожеланий.</div>
