f4b24e8d65d61c0b2fd96c92fe42b519003701ac
Sergei <zubkov@smoothie.digital>
2017-10-11 16:33:02 +0300

    test

--- frontend/static/js/views/CatalogItem.vue
+++ frontend/static/js/views/CatalogItem.vue
@@ -4,11 +4,11 @@
             <div class="item__header">
                 <div class="item__header_title">Оборудование</div>
                 <div class="item__header_name">Световое оборудование</div>
-                <div class="item__header_product item__header_product-new">Clay Paky Scenius Spot</div>
+                <div class="item__header_product " :class="{'item__header_product-new' : result.tag}">{{ result.name }}</div>
             </div>
             <div class="item__content">
                 <div class="item__content_left">
-                    <img src="/static/img/category.jpg" alt="cover" class="item__content_img">
+                    <img :src="result.cover" alt="cover" class="item__content_img">
                     <div class="item__content_video">
                         <div class="item__content_video-text">Демо Видео:</div>
                         <a href="#">
@@ -19,69 +19,25 @@
                         <div class="item__content_project-title">Использован на мероприятиях:</div>
                         <div class="item__content_project-items">
                             <div class="item__content_project-item">Conte Fashion Show</div>
-                            <div class="item__content_project-item">группировка «Ленинград»</div>
-                            <div class="item__content_project-item">Мисс Беларусь – 2016</div>
-                            <div class="item__content_project-item">Серия концертов в Большом концертном зале “Октябрьский”</div>
                         </div>
                     </div>
                 </div>
-                <div class="item__content_right">
-                    <div class="item__content_desc">Scenius – новейший прибор типа “спот” на вершине линейки Clay Paky. Благодаря самой современной системе линз и новой лампе OSRAM на 1400W, этот прибор способен производить луч, значительно превышающий показатели яркости старых 1500-Ваттных приборов. Также впечатляющий диапазон ширины раскрытия луча позволяет применять этот прибор в любой ситуации. Современная система авторегулировки фокуса помогает сохранять максимальную четкость луча при примении эффектов, также есть опция полной автоматизации фокусировки.
-                        Выбирая Scenius Вы будете поражены как настоящим белым светом, так и сочностью каждого из цветовых фильтров. Этот прибор производит белый луч со стабильной цветовой температурой в 6500K, которая не изменяется на протяжении использования лампы. Кроме того, у Scenius крайне высокий показатель индекса цветопередачи, (англ. colour rendering index, CRI — параметр, характеризующий уровень соответствия естественного цвета тела видимому (кажущемуся) цвету этого тела при освещении его данным источником света), что крайне необычно для металл-галогенных ламп. Благодаря независимой ситсеме CMY, цветовому колесу с 7 цветами и линейному фильтру CTO, Scenius может похвастаться широким спектром цветов, включающим также пастельные тона.
-                        Встроенные в прибор эффекты также поддерживают высокий уровень этого прибора: 2 колеса с вращающимися гобо, по 6 сменных гобо в каждом, вращающаяся призма и сменное колесо графических эффектов в своем сочетании позволяют добиваться уникальных визуальных эффектов для любой ситуации. Прибор обладает сверхточной диафрагмой и может быть снабжен современным фрост-фильтром. У Scenius также существует энергосберегающий режим 1200W.
-                    </div>
-                    <div class="item__content_link">Описание на сайте производителя</div>
+                <div class="item__content_right" v-if=" result.producer && result.producer.length !== 0">
+                    <div class="item__content_desc">{{ result.description }}</div>
+                    <a :href="result.producer_link" target="_blank" class="item__content_link">Описание на сайте производителя</a>
                     <div class="item__content_producer">
                         <div class="item__content_producer_title">Производитель</div>
                         <div class="item__content_producer_item">
-                            <img src="/static/img/clay.png" alt="cover" class="item__content_producer_item-img">
+                            <img :src="result.producer.cover" alt="cover" class="item__content_producer_item-img">
                             <div class="item__content_producer_item-text">
-                                <span>Clay Paky </span>
-                                <span>Италия</span>
+                                <span>{{ result.producer.name }}</span>
+                                <span>{{ result.producer.country }}</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!--<div class="item__wrapper">-->
-        <!--<div class="item__header">-->
-        <!--<div class="item__header_title">Оборудование</div>-->
-        <!--<div class="item__header_name">Световое оборудование</div>-->
-        <!--<div class="item__header_product " :class="{'item__header_product-new' : result.tag}">{{ result.name }}</div>-->
-        <!--</div>-->
-        <!--<div class="item__content">-->
-        <!--<div class="item__content_left">-->
-        <!--<img :src="result.cover" alt="cover" class="item__content_img">-->
-        <!--<div class="item__content_video">-->
-        <!--<div class="item__content_video-text">Демо Видео:</div>-->
-        <!--<a href="#">-->
-        <!--<img src="/static/img/youtube.svg" alt="cover" class="item__content_video-img">-->
-        <!--</a>-->
-        <!--</div>-->
-        <!--<div class="item__content_project">-->
-        <!--<div class="item__content_project-title">Использован на мероприятиях:</div>-->
-        <!--<div class="item__content_project-items">-->
-        <!--<div class="item__content_project-item">Conte Fashion Show</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="item__content_right" v-if=" result.producer && result.producer.length !== 0">-->
-        <!--<div class="item__content_desc">{{ result.description }}</div>-->
-        <!--<a :href="result.producer_link" target="_blank" class="item__content_link">Описание на сайте производителя</a>-->
-        <!--<div class="item__content_producer">-->
-        <!--<div class="item__content_producer_title">Производитель</div>-->
-        <!--<div class="item__content_producer_item">-->
-        <!--<img :src="result.producer.cover" alt="cover" class="item__content_producer_item-img">-->
-        <!--<div class="item__content_producer_item-text">-->
-        <!--<span>{{ result.producer.name }}</span>-->
-        <!--<span>{{ result.producer.country }}</span>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
     </div>
 </template>
 
