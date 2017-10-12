7a9117552cd38cbe87e2eb634892d2e31be2384a
Sergei <zubkov@smoothie.digital>
2017-10-12 17:49:46 +0300

    клиенты

--- frontend/static/js/components/MainClient.vue
+++ frontend/static/js/components/MainClient.vue
@@ -2,47 +2,51 @@
     <div class="main__client">
         <div class="main__client_wrapper">
             <div class="main__client_title">Наши клиенты</div>
-            <div class="main__client_items" id="ticker">
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-                <div class="main__client_item">
-                    <img src="/static/img/FIL.png" alt="tape">
-                </div>
-            </div>
+            <swiper :options="swiperOption" class="main__client_items">
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c1.png" alt="tape">
+                </swiper-slide>
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c2.png" alt="tape">
+                </swiper-slide>
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c3.png" alt="tape">
+                </swiper-slide>
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c4.png" alt="tape">
+                </swiper-slide>
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c5.png" alt="tape">
+                </swiper-slide>
+                <swiper-slide class="main__client_item">
+                    <img src="/static/img/client/c6.png" alt="tape">
+                </swiper-slide>
+            </swiper>
         </div>
     </div>
 </template>
 
 <script>
+  import Swiper from "../../../../node_modules/vue-awesome-swiper/src/swiper.vue";
   export default {
     data() {
       return {
-
+        swiperOption: {
+          spaceBetween: 40,
+          slidesPerView: 8,
+          paginationClickable: true,
+          autoplay: 5000,
+          speed: 1000,
+        },
       }
+    },
+    components: {
+      Swiper
     },
     methods: {
-      tiker(){
 
-      }
     },
     mounted() {
-      this.tiker()
     }
   }
 </script>
